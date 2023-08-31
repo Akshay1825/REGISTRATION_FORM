2fe12877e30fa20285afb131e225c1c217020cad
Akshay Utekar <96869967+Akshay1825@users.noreply.github.com>
2023-08-31 22:44:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function register() {
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const userData = `
+        <h2>User Information</h2>
+        <p><strong>First Name:</strong> ${firstName}</p>
+        <p><strong>Last Name:</strong> ${lastName}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Password:</strong> ${password}</p>
+    `;
+
+    document.getElementById("userData").innerHTML = userData;
+}
